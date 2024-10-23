890aad146d422f02074f27fda6cfd542f594d1f4
Nils-Kristian Pedersen <nikrpedev@gmail.com>
2024-10-23 17:36:23 +0200

    updated react routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import Search from './pages/Search.jsx'
+
+const router = createBrowserRouter([{
+  path:'/',
+  element: <Home />,
+  errorElement: <div>404 Page not found, Sorry..</div>
+}, {
+  path:'/search',
+  element: <Search />
+}])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <RouterProvider router={router} />       
   </StrictMode>,
 )
